5cea2b29f2ced91f75d17e5110a7e74604e8bdb5
timiwritescode <odebodezion@gmail.com>
2025-01-14 02:54:36 +0100

    Add test file

--- 04_cut_tool/src/Main.java
+++ 04_cut_tool/src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String filePath = "/home/alterego/Downloads/challenge-cut/fourchords.csv";
+        String filePath = "challenge-cut/fourchords.csv";
         CutTool tool = new CutTool(filePath, Delimeter.COMMA);
 
         tool.handleTheFieldOption(4);
