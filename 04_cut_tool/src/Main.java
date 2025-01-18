9b25f88ac78a408605926319874d890de0c055c1
timiwritescode <odebodezion@gmail.com>
2025-01-18 17:42:41 +0100

    add picocli graavl image integration

--- 04_cut_tool/src/Main.java
+++ 04_cut_tool/src/Main.java
@@ -1,11 +1,34 @@
+import picocli.CommandLine;
+
 import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
 
-        String filePath = "challenge-cut/fourchords.csv";
-        CutTool tool = new CutTool(filePath, Delimeter.COMMA);
+@CommandLine.Command(
+        name = "cc_cut",
+        mixinStandardHelpOptions = true,
+        description = "Linux cut command line tool")
+public class Main implements Runnable {
+    @CommandLine.Option(names = "-f", description = "Specify the column number to pick", required = true)
+    private int column;
 
-        tool.handleTheFieldOption(4);
+
+    @Override
+    public void run() {
+        if (column <= 0) {
+            System.out.println("Error: -f must be a positive integer");
+            System.exit(1);
+        }
+        System.out.println("The number is: " + column);
+
     }
+
+    public static void main(String[] args) throws IOException {
+//
+//        String filePath = "challenge-cut/fourchords.csv";
+//        CutTool tool = new CutTool(filePath, Delimeter.COMMA);
+//
+//        tool.handleTheFieldOption(4);
+        int exitCode = new CommandLine(new Main()).execute(args);
+    }
+
 }
