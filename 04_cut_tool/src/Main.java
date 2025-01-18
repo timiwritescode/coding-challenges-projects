fac98247badb2485c89d79823579aaa042bdee12
timiwritescode <odebodezion@gmail.com>
2025-01-18 18:27:07 +0100

    Add parsing for all f option values

--- 04_cut_tool/src/Main.java
+++ 04_cut_tool/src/Main.java
@@ -1,6 +1,7 @@
 import picocli.CommandLine;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 @CommandLine.Command(
@@ -9,17 +10,45 @@ import java.io.IOException;
         description = "Linux cut command line tool")
 public class Main implements Runnable {
     @CommandLine.Option(names = "-f", description = "Specify the column number to pick", required = true)
-    private int column;
+    private String fOption;
 
+    @CommandLine.Option(names = "d", description = "Specify the delimeter to se")
+    private String delimeter;
 
     @Override
     public void run() {
-        if (column <= 0) {
-            System.out.println("Error: -f must be a positive integer");
+        ArrayList<Integer> options = getFOptionValues();
+
+
+
+    }
+
+    private ArrayList<Integer> getFOptionValues() {
+        ArrayList<Integer> options = new ArrayList<Integer>();
+        System.out.println(fOption);
+        int sum = 0;
+        try{
+            if (fOption.contains(",")) {
+                // split at comma
+                String[] refOptions = fOption.split(",");
+                for (String refOpt: refOptions) {
+                    options.add(Integer.parseInt(refOpt));
+                }
+
+            } else {
+                // split at space
+                String[] refOptions = fOption.split(" ");
+                for (String refOpt: refOptions) {
+                    options.add(Integer.parseInt(refOpt));
+                }
+            }
+
+            return options;
+        } catch (NumberFormatException e) {
+            System.out.println("Error: 0ne or more value of input is not a positive integer");
             System.exit(1);
         }
-        System.out.println("The number is: " + column);
-
+        return new ArrayList<>();
     }
 
     public static void main(String[] args) throws IOException {
