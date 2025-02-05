641e810a95216b473182d1f3443382d6aefa367e
timiwritescode <odebodezion@gmail.com>
2025-02-05 22:36:43 +0100

    init sort tool

---
+++ 06_sort_tool/src/SortTool.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class SortTool {
+    private final String filepath;
+    private List<String> wordList = new ArrayList<>();
+
+    SortTool(String filePath) {
+        this.filepath = filePath;
+
+    }
+
+
+
+}
